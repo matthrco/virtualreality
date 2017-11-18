2eb2d1519115d64a4f2717d5d4b0bfb6633ffc5f
Matt <matthrco@users.noreply.github.com>
2017-11-18 15:37:08 +0000

    Update makery.js

--- makery.js
+++ makery.js
@@ -1,25 +1,11 @@
 /* global AFRAME */
 
-var portals = [];
 var currentPortalName = "#makery1";
-var camera = null;
-var clicked;
-var activePortal = 0;
 var rotations = {"#makery1" : "0 180 0", "#makery2" : "0 70 0", "#makery3" : "0 270 0"};
 
 AFRAME.registerComponent('makery', {
   init: function () {
-    var sceneEl = document.querySelector('a-scene');
-    portals = sceneEl.querySelectorAll('[portal]');
-    var target = document.querySelector('#vid');
-    target.setAttribute('material', 'src: ');
-    target.setAttribute('rotation', rotations[currentPortalName]);
-    target.setAttribute('material', 'src: '+currentPortalName);
-    target.setAttribute('material', 'src', currentPortalName);
-    //portals[activePortal].classList.add("link");
-    portals[activePortal].setAttribute('visible', true);
-    activePortal = (currentPortalName.substring(7)) - 1;
-    goToPortal(activePortal);
+    goToPortal((currentPortalName.substring(7)) - 1);
   }
 });
 
@@ -43,38 +29,42 @@ AFRAME.registerComponent('portal', {
       shader: 'flat', 
       color: '#FFFFFF',
       opacity: 0.7,
-      src: '#gradient',
+      src: '#portalTexture',
       side: 'front',
       transparent: true,
-      depthTest: false
+      depthTest: true
     });
     //this.setupFadeAnimation();
     element.addEventListener('mousedown', function (event) {
       element.setAttribute('material', {
         opacity: 0.7,
-        src: '#gradient',
-        side: 'front'
+        src: '#portalTexture',
+        side: 'front',
+        depthTest: true
       });
     });
     element.addEventListener('mouseup', function (event) {
       element.setAttribute('material', {
         opacity: 1,
-        src: '#gradient',
-        side: 'front'
+        src: '#portalTexture',
+        side: 'front',
+        depthTest: true
       });
     });
     element.addEventListener('mouseenter', function (event) {
       element.setAttribute('material', {
         opacity: 1,
-        src: '#gradient',
-        side: 'front'
+        src: '#portalTexture',
+        side: 'front',
+        depthTest: true
       });
     });
     element.addEventListener('mouseleave', function (event) {
       element.setAttribute('material', {
         opacity: 0.7,
-        src: '#gradient',
-        side: 'front'
+        src: '#portalTexture',
+        side: 'front',
+        depthTest: true
       });
     });
     element.addEventListener('click', function (event) {
@@ -90,28 +80,86 @@ AFRAME.registerComponent('portal', {
       data.target.setAttribute('material', 'src', data.src);
       data.target.setAttribute('position', data.position);
       //portals[activePortal].classList.add("link");
-      portals[activePortal].setAttribute('visible', true);
-      activePortal = (data.src.substring(7)) - 1;
+      sceneEl.querySelector(currentPortalName).setAttribute('visible', true);
       var secondCameraEl = document.querySelector("#camera");
       secondCameraEl.setAttribute('position', data.position);
       secondCameraEl.setAttribute('rotation', data.rotation);
-      goToPortal(activePortal);
+      goToPortal((data.src.substring(7)) - 1);
     });
   }
 });
+
+AFRAME.registerComponent('prompt', {
+  schema: {
+    position: {type: 'string'}
+  },
+  init: function () {
+    // This will be called after the entity has properly attached and loaded.
+    var data = this.data;
+    var element = this.el;
+    var bottom = document.createElement('a-entity');
+    var top = document.createElement('a-entity');
+    bottom.setAttribute('geometry', {
+      primitive: 'cone',
+      radiusTop: 0.2,
+      radiusBottom: 0,
+      height: 0.2,
+      segmentsRadial: 3
+    });
+    bottom.setAttribute('material', {
+      color: 'green'
+    });
+    
+    top.setAttribute('position', "0 0.2 0");
+    top.setAttribute('geometry', {
+      primitive: 'cylinder',
+      radius: 0.05,
+      height: 0.2
+    });
+    top.setAttribute('material', {
+      color: 'green'
+    });
+    
+    element.appendChild(bottom);
+    element.appendChild(top);
+  }
+});
+
 
 function goToPortal(portal){
   var sceneEl = document.querySelector('a-scene');
   var light = document.querySelector('[light]');
   if(portal === 0){
+    var sceneEl = document.querySelector('a-scene');
+    var target = document.querySelector('#vid');
+    target.setAttribute('material', 'src: ');
+    target.setAttribute('rotation', rotations[currentPortalName]);
+    target.setAttribute('material', 'src: '+currentPortalName);
+    target.setAttribute('material', 'src', currentPortalName);
+    //portals[activePortal].classList.add("link");
+    sceneEl.querySelector(currentPortalName).setAttribute('visible', true);
     document.querySelector('#pos1').classList.remove("link");
     document.querySelector('#pos2').classList.remove("link");
     document.querySelector('#pos3').classList.remove("link");
+    var box = document.querySelector("#cover");
+    box.setAttribute('position', "-0.162 -1.655 0.018");
+    box.setAttribute('rotation', "0 90 0");
+    box.setAttribute('scale', "5.370 0 2.56");
+    box.setAttribute('material', {
+      src: '#floorTexture1'
+    });
     light.setAttribute('intensity', 1.3);
   }else if(portal === 1){
     document.querySelector('#pos1').classList.remove("link");
     document.querySelector('#pos2').classList.remove("link");
     document.querySelector('#pos3').classList.remove("link");
+    var box = document.querySelector("#cover");
+    box.setAttribute('position', "-5.134 -1.655 2.179");
+    box.setAttribute('rotation', "-3.037 -25.78 2.922");
+    box.setAttribute('scale', "5.370 0 2.56");
+    box.setAttribute('material', {
+      src: '#floorTexture2'
+    });
     light.setAttribute('intensity', 1);    
   }else if(portal === 2){
     var newVid = document.querySelector("#scratch");
@@ -119,9 +167,16 @@ function goToPortal(portal){
     document.querySelector('#pos1').classList.add("link");
     document.querySelector('#pos2').classList.add("link");
     document.querySelector('#pos3').classList.add("link");
+    var box = document.querySelector("#cover");
+    box.setAttribute('position', "-5.138 -1.655 -2.029");
+    box.setAttribute('rotation', "0 177 0");
+    box.setAttribute('scale', "3.91 0 1.99");
+    box.setAttribute('material', {
+      src: '#floorTexture3'
+    });
     light.setAttribute('intensity', 1);    
   }
   //portals[portal].classList.remove("link");
-  portals[portal].setAttribute('visible', false);
+  sceneEl.querySelector(currentPortalName).setAttribute('visible', false);
   sceneEl.querySelector('[raycaster]').components.raycaster.refreshObjects();
 }
